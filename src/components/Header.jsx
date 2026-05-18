1df7abd0ec378fd39db895e04413493db6fb4e9c
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-05-18 18:24:17 +0200

    ADD Header:  implementazione 2-way Data Binding della searchBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,35 @@
+import { useState } from "react";
+
+const API_URL = 'https://api.themoviedb.org/3/search/movie';
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 // CREARE MODULE.CSS 
 
 function Header() {
-    
-    console.log(API_KEY);
-    
+    // console.log(API_KEY , /* API_URL */);
+    const [search, setSearch] = useState('');
+
+    const searchHandler = (event) => {
+        const target = event.target;
+        const { value } = target;
+        setSearch(value);
+        console.log(search)
+    }
+
 
     return (
         <>
             <h1>Ciao sono l'header. COMPONENTI: searchBar e il suo Button</h1>
 
-            <h1>TITOLo o LOGO</h1>
+            <h1>TITOLO</h1>
             <div className=''>
                 <input
                     type='text'
-                    value={}
-                    onChange={}
+                    value={search}
+                    onChange={searchHandler}
                     placeholder='Cerca un film'
                 />
-                <button onClick='' className=''>Cerca</button>
+                <button>Cerca</button>
             </div>
         </>
     )
